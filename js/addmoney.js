243824602fd0d579ffc77fc9a13a1958a74c8352
unknown <Meherabhasib>
2024-09-25 07:33:47 +0600

    check

--- js/addmoney.js
+++ js/addmoney.js
@@ -66,21 +66,16 @@ document.getElementById('donet-btn-3').addEventListener('click', function(event)
     "bg-white w-5/6 mx-auto rounded-lg border-2 border-slate-300"
     historyItem.innerHTML=`    
     <p class="text-sm font-medium">${new Date().toLocaleDateString()}</p>
-    `
+    <p class="text-sm font-medium">${new Date().toLocaleDateString()}</p>
+    <p class="text-sm font-medium">${new Date().toLocaleDateString()}</p>`
+    
 
     const historyList = document.getElementById('history-donate-dtls')
 
-    historyList.insertBefore(historyItem, history.firstChild )
+    historyList.insertBefore(historyItem, historyList.firstChild )
+
+   
 })
 
-//const historyContainer = document.getElementById('history-btn');
-//const donationContrainer = document.getElementById('donation-btn');
-//historyContainer.addEventListener('click', function () {
 
-  //  historyContainer.classList.add('bg-lime-300')    
-
-    //donationContrainer.classList.remove('bg-lime-300')
-
-    
-//})
 
